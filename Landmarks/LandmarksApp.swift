784f2777dab7d259530a7201c3eac4d8c72f5ef9
Karan Thukral <karanthukral@gmail.com>
2021-03-06 16:40:50 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Karan Thukral on 2021-03-06.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
